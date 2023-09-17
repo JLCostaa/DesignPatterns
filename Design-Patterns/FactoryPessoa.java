fe26ebb9a3f3b31967d36d8208bd9fe8403d2cd0
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-09-17 15:43:54 -0300

    Update FactoryPessoa.java

--- Design-Patterns/FactoryPessoa.java
+++ Design-Patterns/FactoryPessoa.java
@@ -1,1 +1,13 @@
+package principal;
 
+class FactoryPessoa {
+
+    public Pessoa getPessoa(String nome, String sexo) {
+        if (sexo.equals("F")) {
+            return new Mulher(nome);
+        } else {
+            return new Homem(nome);
+        }
+    }
+
+}
