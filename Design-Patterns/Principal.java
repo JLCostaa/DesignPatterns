58916a870d1f666493c0dbe57cee5d4714e0af5e
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-09-17 15:44:35 -0300

    Update Principal.java

--- Design-Patterns/Principal.java
+++ Design-Patterns/Principal.java
@@ -1,1 +1,12 @@
+package principal;
 
+public class Principal {
+
+    public static void main(String[] args) {
+        FactoryPessoa factory = new FactoryPessoa();
+        String nome = "Marina";
+        String sexo = "F";
+        factory.getPessoa(nome, sexo);
+    }
+
+}
