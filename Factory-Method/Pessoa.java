58ab4de5c2513e866536fb0e4be1ba3599ee430b
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-09-17 15:51:48 -0300

    Create Pessoa.java

---
+++ Factory-Method/Pessoa.java
@@ -0,0 +1,24 @@
+package principa;
+
+public abstract class Pessoa {
+
+    public String nome;
+    public String sexo;
+
+}
+class Mulher extends Pessoa {
+
+    public Mulher(String nome) {
+        this.nome = nome;
+        System.out.println("Olá Senhora " + this.nome);
+    }
+
+}
+
+class Homem extends Pessoa {
+
+    public Homem(String nome) {
+        this.nome = nome;
+        System.out.println("Olá Senhor " + this.nome);
+    }
+}
