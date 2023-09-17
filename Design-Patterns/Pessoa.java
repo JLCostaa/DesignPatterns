fa0328918d7db96eb414f1885fab58d16652c581
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-09-17 15:44:18 -0300

    Update Pessoa.java

--- Design-Patterns/Pessoa.java
+++ Design-Patterns/Pessoa.java
@@ -1,1 +1,25 @@
+package principal;
+
+public abstract class Pessoa {
+
+    public String nome;
+    public String sexo;
+
+}
+class Mulher extends Pessoa {
+
+    public Mulher(String nome) {
+        this.nome = nome;
+        System.out.println("Olá Senhora " + this.nome);
+    }
+
+}
+
+class Homem extends Pessoa {
+
+    public Homem(String nome) {
+        this.nome = nome;
+        System.out.println("Olá Senhor " + this.nome);
+    }
+}
 
