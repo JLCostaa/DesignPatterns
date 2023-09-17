2afbcc0d106b00bccc5539584f535b52b86b15cb
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-09-17 15:53:13 -0300

    Create Client.java

---
+++ Padrao-singleton/Client.java
@@ -0,0 +1,10 @@
+package client;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Singleton.getSingleton();
+        Singleton.getSingleton();
+        Singleton.getSingleton();
+    }
+}
