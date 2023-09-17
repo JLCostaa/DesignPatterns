e3dbf49ed59520fe284546223c69169fabbf3d6f
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-09-17 15:54:27 -0300

    Create AImposto.java

---
+++ Padrao-Strategy/AImposto.java
@@ -0,0 +1,11 @@
+
+package calculadorateste;
+
+public abstract class AImposto implements Imposto{
+
+    protected double valor;
+    
+    @Override
+    public abstract double calcular();
+    
+}
